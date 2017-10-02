07a2a71a572db764d8be86a3cfcc0e4eb0258dc6
0x5nc <slim.essency@gmail.com>
2017-10-02 20:45:44 +0100

    QuickUnion Algorithm Implementation.

---
+++ Algorithm Implementations/Algorithm Implementations/QuickFind Algorithms/QuickUnionUF.cs
@@ -0,0 +1,43 @@
+﻿namespace Algorithm_Implementations.QuickFind_Algorithms
+{
+    /*this algorithm is also not a very good implementation on a big scale
+     * it requires N accesses to find the root 
+     * the tree of roots in the table can get really big to the point that finding a root
+     * requires going through the entire variables at worst case */
+    class QuickUnionUF
+    {
+        private int[] id;
+
+        //n array accesses
+        public QuickUnionUF(int n)
+        {
+            id = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                id[i] = i;
+            }
+        }
+
+        private int root(int i)
+        {
+            while (i != id[i])
+            {
+                i = id[i];
+            }
+            return i;
+        }
+
+        public bool Connected(int p, int q)
+        {
+            return root(p) == root(q);
+        }
+
+        public void Union(int p, int q)
+        {
+            int i = root(p);
+            int j = root(q);
+
+            id[i] = j;
+        }
+    }
+}
